7e611f78aa3fb067783312f7114e48341548c5a4
Sebastian-Alvarez <sebastian.alvarezav@usm.cl>
2024-11-24 08:32:37 -0300

    Caledar App added

--- eduplanner_project/settings.py
+++ eduplanner_project/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django Rest Framework
     'rest_framework',
     'requests',
+    # Apps
     'eduplanner',
+    'Calendar',
 ]
 
 MIDDLEWARE = [
