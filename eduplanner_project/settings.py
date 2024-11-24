9d6d91ef33c055e047a6ec442dc36925db829e25
Sebastian-Alvarez <sebastian.alvarezav@usm.cl>
2024-11-24 08:27:21 -0300

    added venv and gitingnore

--- eduplanner_project/settings.py
+++ eduplanner_project/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'eduplanner',
     'rest_framework',
+    'requests',
+    'eduplanner',
 ]
 
 MIDDLEWARE = [
