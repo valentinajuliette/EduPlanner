c9198a18a85bbc50701422e43210a58b19759e81
Sebastian-Alvarez <sebastian.alvarezav@usm.cl>
2024-11-24 20:42:41 -0300

    filters by groups

--- eduplanner/views.py
+++ eduplanner/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth import login, authenticate, logout
 from .models import EventoAcademico
 from .forms import RegistroForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+
 
 def lista_EventoAcademicos(request):
     EventoAcademicos = EventoAcademico.objects.all()
@@ -18,27 +20,37 @@ def pag_principal(request):
     return render(request, 'pag_principal.html', {'EventoAcademicos': EventoAcademicos})
 
 def registro(request):
-    if request.method == 'POST':
-        form = RegistroForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Registro exitoso. Ahora puedes iniciar sesión.')
-            return redirect('login')
+    if request.user.is_authenticated:
+    # Do something for authenticated users.
+        return redirect('pag_principal')
     else:
-        form = RegistroForm()
-    return render(request, 'registro.html', {'form': form})
+            # Do something for anonymous users.
+        if request.method == 'POST':
+            form = RegistroForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Registro exitoso. Ahora puedes iniciar sesión.')
+                return redirect('login')
+        else:
+            form = RegistroForm()
+        return render(request, 'registro.html', {'form': form})
 
 def iniciar_sesion(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('pag_principal')  # Redirige a la página principal después del login.
-        else:
-            messages.error(request, 'Usuario o contraseña incorrectos.')
-    return render(request, 'login.html')
+    if request.user.is_authenticated:
+        # Do something for authenticated users.
+        return redirect('pag_principal')
+    else:
+        # Do something for anonymous users.
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('pag_principal')  # Redirige a la página principal después del login.
+            else:
+                messages.error(request, 'Usuario o contraseña incorrectos.')
+        return render(request, 'login.html')
 
 def cerrar_sesion(request):
     logout(request)
@@ -86,49 +98,64 @@ class CalendarioAPIView(APIView):
         return Response(calendario_ordenado, status=status.HTTP_200_OK)
     
 def agregar_evento(request):
-    if request.method == 'POST':
-        # Recibir datos del formulario
-        titulo = request.POST.get('titulo')
-        descripcion = request.POST.get('descripcion')
-        fecha_inicio = request.POST.get('fecha_inicio')
-        fecha_fin = request.POST.get('fecha_fin')
+    if request.user.groups.filter(name='Académicos').exists() or request.user.groups.filter(name='Comité Académico').exists():
+        if request.method == 'POST':
+            # Recibir datos del formulario
+            titulo = request.POST.get('titulo')
+            descripcion = request.POST.get('descripcion')
+            fecha_inicio = request.POST.get('fecha_inicio')
+            fecha_fin = request.POST.get('fecha_fin')
 
-        # Crear el nuevo evento
-        nuevo_evento = EventoAcademico(
-            titulo=titulo,
-            descripcion=descripcion,
-            fecha_inicio=fecha_inicio,
-            fecha_fin=fecha_fin
-        )
-        nuevo_evento.save()
+            # Crear el nuevo evento
+            nuevo_evento = EventoAcademico(
+                titulo=titulo,
+                descripcion=descripcion,
+                fecha_inicio=fecha_inicio,
+                fecha_fin=fecha_fin
+            )
+            nuevo_evento.save()
 
-        # Redirigir al calendario o página principal
-        return redirect('calendar_view')  # Cambiar a la ruta que desees
+            # Redirigir al calendario o página principal
+            return redirect('calendar_view')  # Cambiar a la ruta que desees
 
-    # Renderizar la página del formulario
-    return render(request, 'agregar_evento.html')
+        # Renderizar la página del formulario
+        return render(request, 'agregar_evento.html')
+    else:
+        return redirect('pag_principal')
 
+@login_required
 def panel_admin(request):
     # Obtener todos los eventos
-    eventos = EventoAcademico.objects.all()
-    return render(request, 'panel_admin.html', {'eventos': eventos})
+    if request.user.groups.filter(name='Académicos').exists() or request.user.groups.filter(name='Comité Académico').exists():
+        eventos = EventoAcademico.objects.all()
+        return render(request, 'panel_admin.html', {'eventos': eventos})
+    else:
+        return redirect('pag_principal')
 
+@login_required
 def editar_evento(request, evento_id):
     # Obtener el evento a editar
-    evento = get_object_or_404(EventoAcademico, id=evento_id)
-    if request.method == 'POST':
-        evento.titulo = request.POST.get('titulo')
-        evento.descripcion = request.POST.get('descripcion')
-        evento.fecha_inicio = request.POST.get('fecha_inicio')
-        evento.fecha_fin = request.POST.get('fecha_fin')
-        evento.save()
-        return redirect('panel_admin')
-    return render(request, 'editar_evento.html', {'evento': evento})
+    if request.user.groups.filter(name='Académicos').exists() or request.user.groups.filter(name='Comité Académico').exists():
+        evento = get_object_or_404(EventoAcademico, id=evento_id)
+        if request.method == 'POST':
+            evento.titulo = request.POST.get('titulo')
+            evento.descripcion = request.POST.get('descripcion')
+            evento.fecha_inicio = request.POST.get('fecha_inicio')
+            evento.fecha_fin = request.POST.get('fecha_fin')
+            evento.save()
+            return redirect('panel_admin')
+        return render(request, 'editar_evento.html', {'evento': evento})
+    else:
+        return redirect('pag_principal')
 
+@login_required
 def eliminar_evento(request, evento_id):
-    # Obtener el evento y eliminarlo
-    evento = get_object_or_404(EventoAcademico, id=evento_id)
-    if request.method == 'POST':
-        evento.delete()
-        return redirect('panel_admin')
-    return render(request, 'eliminar_evento.html', {'evento': evento})
+    if request.user.groups.filter(name='Académicos').exists() or request.user.groups.filter(name='Comité Académico').exists():
+        # Obtener el evento y eliminarlo
+        evento = get_object_or_404(EventoAcademico, id=evento_id)
+        if request.method == 'POST':
+            evento.delete()
+            return redirect('panel_admin')
+        return render(request, 'eliminar_evento.html', {'evento': evento})
+    else:
+        return redirect('pag_principal')
