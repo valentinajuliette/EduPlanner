a86d40fdddc79b9cd780a2e7dc9ad2e91d5cecfb
Sebastian-Alvarez <sebastian.alvarezav@usm.cl>
2024-11-25 10:45:38 -0300

    fix confidencial en API

--- api/views.py
+++ api/views.py
@@ -6,10 +6,7 @@ from .serializers import EventoSerializer
 # Create your views here.
 
 class EventosViewSets(viewsets.ModelViewSet):
+    queryset = EventosAcademicos.objects.filter(confidencial=False)
     serializer_class = EventoSerializer
 
-    def get_queryset(self):
-        if self.request.user.groups.filter(name='Académicos').exists() or self.request.user.groups.filter(name='Comité Académico').exists():
-            return EventosAcademicos.objects.all()
-        else:
-            return EventosAcademicos.objects.filter(confidencial=False)
+        
