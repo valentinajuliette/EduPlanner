b549073f6f7ab80d74ff12e847967541e5e2cd67
Valita <valita.universitaria@gmail.com>
2024-11-25 09:43:27 -0300

    filtro confidencial

--- Calendar/models.py
+++ Calendar/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 class EventosAcademicos(models.Model):
     LISTA_TIPOS = {
-        "1": "Inicio de Semestre",
-        "2": "Fin de Semestre",
-        "3": "Inicio de Inscripción de Asignaturas",
-        "4": "Fin de Inscripción de Asignaturas",
-        "5": "Receso Académico",
-        "6": "Feriado Nacional", # Default 4 Feriados
-        "7": "Feriado Regional",
-        "8": "Inicio de Plazos de Solicitudes Administrativas",
-        "9": "Fin de Plazos de Solicitudes Administrativas",
-        "10": "Inicio de Plazos para la Gestión de Beneficios",
-        "11": "Fin de Plazos para la Gestión de Beneficios",
-        "12": "Ceremonia de Titulación o Graduación",
-        "13": "Reunión de Consejo Académico",
-        "14": "Talleres y Charlas",
-        "15": "Día de Orientación para Nuevos Estudiantes",
-        "16": "Eventos Extracurriculares",
-        "17": "Inicio de Clases",
-        "18": "Último Día de Clases",
-        "19": "Día de Puertas Abiertas",
-        "20": "Suspensión de Actividades Completa",
-        "21": "Suspensión de Actividades Parcial",
+        "1": "1. Inicio de Semestre",
+        "2": "2. Fin de Semestre",
+        "3": "3. Inicio de Inscripción de Asignaturas",
+        "4": "4. Fin de Inscripción de Asignaturas",
+        "5": "5. Receso Académico",
+        "6": "6. Feriado Nacional", # Default 4 Feriados
+        "7": "7. Feriado Regional",
+        "8": "8. Inicio de Plazos de Solicitudes Administrativas",
+        "9": "9. Fin de Plazos de Solicitudes Administrativas",
+        "10": "10. Inicio de Plazos para la Gestión de Beneficios",
+        "11": "11. Fin de Plazos para la Gestión de Beneficios",
+        "12": "12. Ceremonia de Titulación o Graduación",
+        "13": "13. Reunión de Consejo Académico",
+        "14": "14. Talleres y Charlas",
+        "15": "15. Día de Orientación para Nuevos Estudiantes",
+        "16": "16. Eventos Extracurriculares",
+        "17": "17. Inicio de Clases",
+        "18": "18. Último Día de Clases",
+        "19": "19. Día de Puertas Abiertas",
+        "20": "20. Suspensión de Actividades Completa",
+        "21": "21. Suspensión de Actividades Parcial",
     }
 
     titulo = models.CharField(max_length=200)
